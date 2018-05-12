fc2d0adade6dcc317c6e46ea38dc974ad7c39576
Kelvin Salton do Prado <kelvinpfw@gmail.com>
2018-05-12 19:01:05 -0300

    Add Mediator design pattern

---
+++ comportamentais/mediator/README.md
@@ -0,0 +1,8 @@
+
+# Mediator
+
+O padrão Mediator define um objeto que encapsula a lógica de comunicação entre um conjunto de objetos. Ele basicamente adiciona um terceiro objeto (mediador) que controla a interação entre dois objetos, assim cada objeto não precisa ter conhecimento da implementação do outro objeto. 
+
+## Quando usar?
+
+Quando precisamos realizar uma interação/comunicação entre dois ou mais objetos.
---
+++ comportamentais/mediator/mediator.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+
+from datetime import datetime
+
+from abc import ABCMeta, abstractmethod
+
+
+class ChatRoomMediator(object):
+
+    __metaclass__ = ABCMeta
+
+    @abstractmethod
+    def show_message(self, user, message):
+        pass
+
+
+class ChatRoom(ChatRoomMediator):
+    """Mediator"""
+
+    def show_message(self, user, message):
+        time = datetime.now()
+        sender = user.name
+
+        print '{} [{}]: {}'.format(time, sender, message)
+
+
+class User(object):
+
+    def __init__(self, name, chat_mediator):
+        self.name = name
+        self.chat_mediator = chat_mediator
+
+    def send(self, message):
+        self.chat_mediator.show_message(self, message)
+
+
+if __name__ == '__main__':
+
+    mediator = ChatRoom()
+
+    john = User('John', mediator)
+    jane = User('Jane', mediator)
+    josh = User('Josh', mediator)
+
+    john.send('Hi there!')
+    jane.send('Hi!')
+    john.send('How are you?')
+    jane.send('I\'m great, thanks!')
+    josh.send('Hi guys!')
