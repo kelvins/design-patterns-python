590d3f33987b7600a7ead604a71ba97848a76b93
Kelvin Salton do Prado <kelvinpfw@gmail.com>
2018-08-12 12:29:30 -0300

    Add pattern flyweight

---
+++ estruturais/flyweight/README.md
@@ -0,0 +1,9 @@
+
+# Flyweight
+
+O `flyweight` é um padrão que tem como objetivo minimizar o uso de memória ou custos computacionais compartilhando a maior quantidade de dados entre objetos similares.
+
+## Quando usar?
+
+Quando queremos minimizar o uso de recursos computacionais, compartilhando os dados ou os objetos.
+Considere uma loja que vende chás. A pessoa responsável por fazer o chá geralmente faz mais do que uma xícara de chá e guarda o resto para vender para outros consumidores, poupando assim recursos como gás, entre outros. O padrão `flyweight` é exatamente sobre isso, poupar recursos.
---
+++ estruturais/flyweight/flyweight.py
@@ -0,0 +1,52 @@
+# -*- encoding: UTF-8 -*-
+
+class KarakTea(object):
+
+    def __init__(self, tea_type):
+        self.__tea_type = tea_type
+
+    @property
+    def tea_type(self):
+        return self.__tea_type
+
+
+class TeaMaker(object):
+
+    def __init__(self):
+        self.__available_tea = dict()
+
+    def make(self, preference):
+
+        if preference not in self.__available_tea:
+            self.__available_tea[preference] = KarakTea(preference)
+
+        return self.__available_tea[preference]
+
+
+class TeaShop(object):
+
+    def __init__(self, tea_maker):
+        self.__orders = dict()
+        self.__tea_maker = tea_maker
+
+    def take_order(self, tea_type, table):
+
+        if table not in self.__orders:
+            self.__orders[table] = list()
+
+        self.__orders[table].append(self.__tea_maker.make(tea_type))
+
+    def serve(self):
+        for table, orders in self.__orders.iteritems():
+            print u'Serving tea to table {}'.format(table)
+
+if __name__ == u'__main__':
+    tea_maker = TeaMaker()
+    shop = TeaShop(tea_maker)
+
+    shop.take_order(u'red tea', 1)
+    shop.take_order(u'red tea more sugar', 2)
+    shop.take_order(u'red tea more milk', 3)
+
+    shop.serve()
+
