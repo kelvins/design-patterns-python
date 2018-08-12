aa43fbaea2aaec83e858ef37cc4ed079e4d6ccf6
Kelvin Salton do Prado <kelvinpfw@gmail.com>
2018-08-12 12:00:21 -0300

    Add composite pattern

---
+++ estruturais/composite/README.md
@@ -0,0 +1,10 @@
+
+# Composite
+
+O padrão `composite` permite tratar objetos individuais de forma uniforme. Ele basicamente descreve que um grupo de objetos devem ser tratados da mesma forma que uma instância única de um objeto.
+
+O propósito do padrão `composite` é compor objetos em estruturas de árvores que representam hierarquias.
+
+# Quando usar?
+
+Todas as organizações são compostas por colaboradores. Cada colaborador possui as mesmas características como salário, responsabilidades, pode ou não ter algum superior, pode ou não ter subordinados.
---
+++ estruturais/composite/composite.py
@@ -0,0 +1,62 @@
+# -*- encoding: UTF-8 -*-
+
+
+class Colaborador(object):
+
+    def __init__(self, nome, salario):
+        self.__nome = nome
+        self.salario = salario
+
+    @property
+    def nome(self):
+        return self.__nome
+
+
+class Desenvolvedor(Colaborador):
+
+    def __init__(self, nome, salario, outro_parametro=None):
+        super(Desenvolvedor, self).__init__(nome, salario)
+        self.__outro_parametro = outro_parametro
+
+    @property
+    def outro_parametro(self):
+        return self.__outro_parametro
+
+
+class Designer(Colaborador):
+
+    def __init__(self, nome, salario, mais_um_parametro=None):
+        super(Designer, self).__init__(nome, salario)
+        self.__mais_um_parametro = mais_um_parametro
+
+    @property
+    def mais_um_parametro(self):
+        return self.__mais_um_parametro
+
+
+class Organizacao(object):
+
+    def __init__(self):
+        self.__colaboradores = list()
+
+    def add_colaborador(self, colaborador):
+        self.__colaboradores.append(colaborador)
+
+    def total_salarios(self):
+        salarios = 0
+
+        for colaborador in self.__colaboradores:
+            salarios += colaborador.salario
+
+        return salarios
+
+
+if __name__ == u'__main__':
+    joao = Desenvolvedor(u'Joao da Silva', 1800)
+    carla = Designer(u'Carla Camila', 1900)
+
+    organizacao = Organizacao()
+    organizacao.add_colaborador(joao)
+    organizacao.add_colaborador(carla)
+
+    print u'Total salários: {}'.format(organizacao.total_salarios())
